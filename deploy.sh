82a255f2ffe608db631abd22692f2626d21b7a91
Katharine Cordes <gillis.katharine@gmail.com>
2021-09-12 18:01:21 -0600

    fix: push composer name env to its own line

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,10 @@ sed -i "s/GID=1000/GID=$GID/g" .env
 sed -i "s/SITE_URL=externals.katiecordescodes.docker/SITE_URL=$SITE_URL/g" .env
 sed -i "s/APP_ENV=dev/APP_ENV=prod/g" .env
 sed -i "s/AAPP_SECRET=\$ecretf0rt3st/APP_SECRET=$APP_SECRET/g" .env
-echo "COMPOSE_PROJECT_NAME=personalsite-externals-$STAGING" >> .env
+{
+  echo ""
+  echo "COMPOSE_PROJECT_NAME=personalsite-externals-$STAGING"
+} >> .env
 
 # Update staging
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml -f "../docker-compose.$STAGING.yml" pull
