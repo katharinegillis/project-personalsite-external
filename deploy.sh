3070983d9acd53318b61378a8f8e74857826ba2c
Katharine Cordes <gillis.katharine@gmail.com>
2021-10-10 18:23:29 -0600

    refactor: fix up docker as per changes on auth

--- deploy.sh
+++ deploy.sh
@@ -8,9 +8,16 @@ CURRENT_DIR=${PWD##*/}
 
 echo "COMPOSE_PROJECT_NAME=${PARENT_PWD##*/}-${CURRENT_DIR}" >> .env
 
+if [[ ! -d var ]]; then
+    mkdir var
+fi
+
 # Update instance
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml -f docker-compose.ssl.yml pull
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml -f docker-compose.ssl.yml up -d --remove-orphans
+
+# Clear the cache
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml -f docker-compose.ssl.yml exec -T app php bin/console clear:cache
 
 # Clean up old networks and images
 docker system prune -f
